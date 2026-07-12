180eb0a558b0eabf85ab447fe12f333a2b3ab880
Xanderewa <149902647+Xanderewa@users.noreply.github.com>
2026-07-12 17:03:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-.
+from config import Config
+from src.signal_manager import SignalManager
+from src.logger import BotLogger
+
+def main():
+    config = Config()
+    logger = BotLogger()
+    manager = SignalManager(config=config, logger=logger)
+    manager.run()
+
+if __name__ == "__main__":
+    main().
