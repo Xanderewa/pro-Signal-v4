b37a34bb1dd7261d127cadc7bbd7eeaf1c9b0c0a
Xanderewa <149902647+Xanderewa@users.noreply.github.com>
2026-07-12 17:02:27 -0300

    Update signal_manager.py

--- src/src/signal_manager.py
+++ src/src/signal_manager.py
@@ -1,1 +1,10 @@
+from src.strategies import StrategyEngine
 
+class SignalManager:
+    def __init__(self, config, logger):
+        self.config = config
+        self.logger = logger
+        self.engine = StrategyEngine()
+
+    def run(self):
+        self.logger.info("Signal manager started")
