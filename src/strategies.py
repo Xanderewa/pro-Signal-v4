64eb1e5b21931c1fdc3a3f0ea1c53c8457c8b54f
Xanderewa <149902647+Xanderewa@users.noreply.github.com>
2026-07-12 17:02:06 -0300

    Update strategies.py

--- src/strategies.py
+++ src/strategies.py
@@ -1,1 +1,9 @@
+class StrategyEngine:
+    def aggressive(self, data, indicators):
+        return None
 
+    def normal(self, data, indicators):
+        return None
+
+    def passive(self, data, indicators):
+        return None
